3fff9d0949772666774d3aad78e232d26bf75760
8bulbs <286630571@qq.com>
2018-02-27 21:59:18 +0800

    Access-Control-Allow-Origin:*

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const EXPRESS = require('express')
 const PATH = require('path')
-let app =new EXPRESS()
+let app = EXPRESS()
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "X-Requested-With")
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS")
+  res.header("X-Powered-By",' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8")
+  next()
+})
 
 app.get('/test',(req, res) => {
-    if(err)throw err
-    res.send('test')
+    console.log('test1')
+    res.send('test1')
 })
 
 app.use(EXPRESS.static(PATH.join(__dirname, '/dist')))
+
 
 app.listen(8007, (err) => {
   if(err)throw err
