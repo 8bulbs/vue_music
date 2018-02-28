8cabee8ac400d81f354789a225b10d6b4e419efc
8bulbs <286630571@qq.com>
2018-02-28 14:04:21 +0800

    server

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 const EXPRESS = require('express')
 const PATH = require('path')
+
 let app = EXPRESS()
+
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "X-Requested-With")
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS")
-  res.header("X-Powered-By",' 3.2.1')
-  res.header("Content-Type", "application/json;charset=utf-8")
   next()
 })
 
 app.get('/test',(req, res) => {
-    console.log('test1')
-    res.send('test1')
+    console.log('test')
+    res.send('test')
 })
 
 app.use(EXPRESS.static(PATH.join(__dirname, '/dist')))
 
+// app.use(cors({
+//   origin:["*"],
+//   credentials:true
+// }))
 
-app.listen(8007, (err) => {
+app.listen(8001, (err) => {
   if(err)throw err
-  console.log('listen at 8007 port')
+  console.log('listen at 8001 port')
 })
 
 
